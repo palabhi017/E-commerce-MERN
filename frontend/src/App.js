37695c84e03a973ff96ffb948966e8059962b199
RajatMujawar123git add.git commit -m evaluationgit config --global user.email mujawarrajat009@gmail.comgit config --global user.name RajatMujawar123 <mujawarrajat009@gmail.com>
2023-03-31 23:07:37 +0530

    add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import Allroutes from './Pages/Allroutes';
+import Homepage from './Pages/Homepage';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-     <Allroutes/>
+      <Homepage/>
+     {/* <Allroutes/> */}
      <Footer/>
+   
     </div>
   );
 }
