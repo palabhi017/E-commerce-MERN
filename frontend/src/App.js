4b8e33f7100b2a8326b8777b0fc97436b630491c
soniadiwedi <112754761+soniadiwedi@users.noreply.github.com>
2023-03-31 11:35:35 +0530

    single

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,13 @@
 
 import logo from "./logo.svg";
 import "./App.css";
+import Singleproductpage from "./Pages/Singleproductpage";
 
 
 function App() {
   return (
     <div className="App">
-
+      
     </div>
   );
 }
--- frontend/src/Pages/Allroutes.jsx
+++ frontend/src/Pages/Allroutes.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Routes,Route} from "react-router-dom"
 import Productpage from './Productpage'
+import Singleproductpage from './Singleproductpage'
 const Allroutes = () => {
   return (
     <Routes>
           <Route path="/product" element={<Productpage/>}/>
+          <Route path="/product/:id" element={<Singleproductpage/>} />
     </Routes>
   )
 }
